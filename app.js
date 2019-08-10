797eae6031959cb1240ca28e81daf3dc5ec13bfa
Snir Holland <sniro84@hotmail.com>
2019-08-10 14:20:00 +0300

    added project routes

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const data = require('./data/data.json');
+const projects = data.projects;
 
 const app = express();
 
+app.set('view engine' , 'pug');
 
-app.listen(3000);
+app.use('/static',express.static('public'));
+
+app.listen(3000 , () => {
+    console.log("Listening to port 3000...");
+});
 
 app.get('/' , (req,res) => {
-    res.send('App works');
+    res.render('index');
+    res.locals.projects = projects;
+    console.log(res.locals.projects);
+});
+
+app.get('/about' , (req,res) => {
+    res.render('about');
+});
+
+app.get('/projects/:id' , (req,res) => {
+    res.render('project' , {
+        id: projects[req.params.id].id ,
+        name: projects[req.params.id].project_name
+    });
+
+    // why is it null ?
+    console.log(res.locals);
+
 });
