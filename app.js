6bc7f52477f190681bf37165ae66bb34ba515587
Snir Holland <sniro84@hotmail.com>
2019-08-11 19:17:36 +0300

    changed css styles

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use( (req,res,next) => {
 app.use( (err,req,res,next) => {
     res.locals.error = err;
     res.status(err.status);
-    res.render('error');
+    res.render('error');  
 });
 
 app.listen(3000 , () => {
